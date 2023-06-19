d067c32bda92ceab8ddc03ee2a4fc7bcd0b4d332
Lien0 <lic0231@gmail.com>
2023-06-19 01:36:12 -0600

    ver 1.04

--- Calculadora_All/javascript/script.js
+++ Calculadora_All/javascript/script.js
@@ -29,6 +29,7 @@ var flagResta = 0;
 var flagMult = 0;
 var flagDiv = 0;
 var flagPor = 0;
+var flagFloat = 0;
 
 function number1() {
   scree.insertAdjacentText("beforeend", "1");
@@ -77,91 +78,223 @@ nine.addEventListener("click", number9);
 zero.addEventListener("click", number0);
 dot.addEventListener("click", numberdot);
 
-equal.addEventListener("click", () => {
-  i = true;
-  if (a >= 2) {
-    let b = toString(flagSuma);
-    document.getElementById("scrcalc").innerHTML = b;
-  }
-});
-
 function adicion() {
   a++;
-  if (a == 1) {
-    let b = document.getElementById("scrcalc");
-    flagSuma = parseInt(b.value);
-    document.getElementById("scrcalc").innerHTML = "";
+  let screen = document.getElementById("scrcalc");
+  let string = screen.value;
+  for (let count = 0; count < string.length; count++) {
+    if (string.charAt(count) == ".") {
+      flagFloat++;
+    }
   }
-  if (a >= 2) {
-    let b = document.getElementById("scrcalc");
-    let c = parseInt(b.value);
-    let d = flagSuma + c;
-    flagSuma = d;
-    document.getElementById("scrcalc").innerHTML = "";
+
+  if (flagFloat == 0) {
+    if (a == 1) {
+      let b = document.getElementById("scrcalc");
+      flagSuma = parseInt(b.value);
+      document.getElementById("scrcalc").innerHTML = "";
+    }
+    if (a >= 2) {
+      let b = document.getElementById("scrcalc");
+      let c = parseInt(b.value);
+      let d = flagSuma + c;
+      flagSuma = d;
+      document.getElementById("scrcalc").innerHTML = "";
+    }
+  } else if (flagFloat == 1) {
+    if (a == 1) {
+      let b = document.getElementById("scrcalc");
+      flagSuma = parseFloat(b.value);
+      document.getElementById("scrcalc").innerHTML = "";
+      flagFloat = 0;
+    }
+    if (a >= 2) {
+      let b = document.getElementById("scrcalc");
+      let c = parseFloat(b.value);
+      let d = flagSuma + c;
+      flagSuma = d;
+      document.getElementById("scrcalc").innerHTML = "";
+    }
+  } else if (flagFloat > 1) {
+    alert("Syntax Error: Reload the page");
   }
 }
 
 function sustraccion() {
   r++;
-  if (r == 1) {
-    let b = document.getElementById("scrcalc");
-    flagResta = parseInt(b.value);
-    document.getElementById("scrcalc").innerHTML = "";
+  let screen = document.getElementById("scrcalc");
+  let string = screen.value;
+  for (let count = 0; count < string.length; count++) {
+    if (string.charAt(count) == ".") {
+      flagFloat++;
+    }
   }
-  if (r >= 2) {
-    let b = document.getElementById("scrcalc");
-    let c = parseInt(b.value);
-    let d = flagResta - c;
-    flagResta = d;
-    document.getElementById("scrcalc").innerHTML = "";
+
+  if (flagFloat == 0) {
+    if (r == 1) {
+      let b = document.getElementById("scrcalc");
+      flagResta = parseInt(b.value);
+      document.getElementById("scrcalc").innerHTML = "";
+    }
+    if (r >= 2) {
+      let b = document.getElementById("scrcalc");
+      let c = parseInt(b.value);
+      let d = flagResta - c;
+      flagResta = d;
+      document.getElementById("scrcalc").innerHTML = "";
+    }
+  }
+
+  if (flagFloat == 1) {
+    if (r == 1) {
+      let b = document.getElementById("scrcalc");
+      flagResta = parseFloat(b.value);
+      document.getElementById("scrcalc").innerHTML = "";
+      flagFloat = 0;
+    }
+    if (r >= 2) {
+      let b = document.getElementById("scrcalc");
+      let c = parseFloat(b.value);
+      let d = flagResta - c;
+      flagResta = d;
+      document.getElementById("scrcalc").innerHTML = "";
+    }
+  }
+
+  if (flagFloat > 1) {
+    alert("Syntax Error: Reload the page");
   }
 }
 
 function multiplicacion() {
   m++;
-  if (m == 1) {
-    let b = document.getElementById("scrcalc");
-    flagMult = parseInt(b.value);
-    document.getElementById("scrcalc").innerHTML = "";
+  let screen = document.getElementById("scrcalc");
+  let string = screen.value;
+  for (let count = 0; count < string.length; count++) {
+    if (string.charAt(count) == ".") {
+      flagFloat++;
+    }
   }
-  if (m >= 2) {
-    let b = document.getElementById("scrcalc");
-    let c = parseInt(b.value);
-    let d = flagMult * c;
-    flagMult = d;
-    document.getElementById("scrcalc").innerHTML = "";
+
+  if (flagFloat == 0) {
+    if (m == 1) {
+      let b = document.getElementById("scrcalc");
+      flagMult = parseInt(b.value);
+      document.getElementById("scrcalc").innerHTML = "";
+    }
+    if (m >= 2) {
+      let b = document.getElementById("scrcalc");
+      let c = parseInt(b.value);
+      let d = flagMult * c;
+      flagMult = d;
+      document.getElementById("scrcalc").innerHTML = "";
+    }
+  }
+  if (flagFloat == 1) {
+    if (m == 1) {
+      let b = document.getElementById("scrcalc");
+      flagMult = parseFloat(b.value);
+      document.getElementById("scrcalc").innerHTML = "";
+      flagFloat = 0;
+    }
+    if (m >= 2) {
+      let b = document.getElementById("scrcalc");
+      let c = parseFloat(b.value);
+      let d = flagMult * c;
+      flagMult = d.toFixed(4);
+      document.getElementById("scrcalc").innerHTML = "";
+    }
+  }
+  if (flagFloat > 1) {
+    alert("Syntax Error: Reload the page");
   }
 }
 
 function division() {
   di++;
-  if (di == 1) {
-    let b = document.getElementById("scrcalc");
-    flagDiv = parseInt(b.value);
-    document.getElementById("scrcalc").innerHTML = "";
+  let screen = document.getElementById("scrcalc");
+  let string = screen.value;
+  for (let count = 0; count < string.length; count++) {
+    if (string.charAt(count) == ".") {
+      flagFloat++;
+    }
   }
-  if (di >= 2) {
-    let b = document.getElementById("scrcalc");
-    let c = parseInt(b.value);
-    let d = flagDiv / c;
-    flagDiv = d;
-    document.getElementById("scrcalc").innerHTML = "";
+
+  if (flagFloat == 0) {
+    if (di == 1) {
+      let b = document.getElementById("scrcalc");
+      flagDiv = parseInt(b.value);
+      document.getElementById("scrcalc").innerHTML = "";
+    }
+    if (di >= 2) {
+      let b = document.getElementById("scrcalc");
+      let c = parseInt(b.value);
+      let d = flagDiv / c;
+      flagDiv = d;
+      document.getElementById("scrcalc").innerHTML = "";
+    }
+  }
+  if (flagFloat == 1) {
+    if (di == 1) {
+      let b = document.getElementById("scrcalc");
+      flagDiv = parseFloat(b.value);
+      document.getElementById("scrcalc").innerHTML = "";
+      flagFloat = 0;
+    }
+    if (di >= 2) {
+      let b = document.getElementById("scrcalc");
+      let c = parseFloat(b.value);
+      let d = flagDiv / c;
+      flagDiv = d.toFixed(4);
+      document.getElementById("scrcalc").innerHTML = "";
+    }
+  }
+  if (flagFloat > 1) {
+    alert("Syntax Error: Reload the page");
   }
 }
 
 function porcentaje() {
   p++;
-  if (p == 1) {
-    let b = document.getElementById("scrcalc");
-    flagPor = parseInt(b.value);
-    document.getElementById("scrcalc").innerHTML = "";
+  let screen = document.getElementById("scrcalc");
+  let string = screen.value;
+  for (let count = 0; count < string.length; count++) {
+    if (string.charAt(count) == ".") {
+      flagFloat++;
+    }
   }
-  if (p >= 2) {
-    let b = document.getElementById("scrcalc");
-    let c = parseInt(b.value);
-    let d = (flagPor * c) / 100;
-    flagPor = d;
-    document.getElementById("scrcalc").innerHTML = "";
+
+  if (flagFloat == 0) {
+    if (p == 1) {
+      let b = document.getElementById("scrcalc");
+      flagPor = parseInt(b.value);
+      document.getElementById("scrcalc").innerHTML = "";
+    }
+    if (p >= 2) {
+      let b = document.getElementById("scrcalc");
+      let c = parseInt(b.value);
+      let d = (flagPor * c) / 100;
+      flagPor = d;
+      document.getElementById("scrcalc").innerHTML = "";
+    }
+  }
+  if (flagFloat == 1) {
+    if (p == 1) {
+      let b = document.getElementById("scrcalc");
+      flagPor = parseFloat(b.value);
+      document.getElementById("scrcalc").innerHTML = "";
+      flagFloat = 0;
+    }
+    if (p >= 2) {
+      let b = document.getElementById("scrcalc");
+      let c = parseFloat(b.value);
+      let d = (flagPor * c) / 100;
+      flagPor = d.toFixed(4);
+      document.getElementById("scrcalc").innerHTML = "";
+    }
+    if (flagFloat > 1) {
+      alert("Syntax Error: Reload the page");
+    }
   }
 }
 
@@ -169,25 +302,30 @@ function igual() {
   if (a >= 2) {
     document.getElementById("scrcalc").innerHTML = flagSuma;
     flagSuma = 0;
+    flagFloat = 0;
     a = 0;
   }
   if (r >= 2) {
     document.getElementById("scrcalc").innerHTML = flagResta;
+    flagFloat = 0;
     flagResta = 0;
     r = 0;
   }
   if (m >= 2) {
     document.getElementById("scrcalc").innerHTML = flagMult;
+    flagFloat = 0;
     flagMult = 0;
     m = 0;
   }
   if (di >= 2) {
     document.getElementById("scrcalc").innerHTML = flagDiv;
+    flagFloat = 0;
     flagDiv = 0;
     di = 0;
   }
   if (p >= 2) {
     document.getElementById("scrcalc").innerHTML = flagPor + "%";
+    flagFloat = 0;
     flagPor = 0;
     p = 0;
   }
@@ -203,6 +341,11 @@ equal.addEventListener("click", igual);
 
 function deleteAll() {
   document.getElementById("scrcalc").innerHTML = "";
+  flagFloat = 0;
   flagSuma = 0;
   a = 0;
+  r = 0;
+  di = 0;
+  m = 0;
+  p = 0;
 }
